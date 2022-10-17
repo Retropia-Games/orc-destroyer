61dd0f4f6a71b86b515668168e31b1d850c7ad80
Konrad Fedorczyk <konrad.fedorczyk@oko.press>
2022-10-17 23:16:23 +0200

    Add death scene

--- scenes/enemy-soldier.gd
+++ scenes/enemy-soldier.gd
@@ -4,9 +4,10 @@ export var shots_count = 10
 export var move_speed = 10
 var counter = 0
 
-var explosion_scene = preload("res://scenes/explosion.tscn")
+var explosion_scene = preload("res://scenes/enemy-death.tscn")
 var shot_scene = preload("res://scenes/enemy-bullet.tscn")
-
+var casing_scene = preload("res://scenes/casing.tscn")
+var rng = RandomNumberGenerator.new()
 
 var score_emitted = false
 
@@ -39,16 +40,23 @@ func _on_shot_timer_timeout():
 	
 	if counter < shots_count:
 		shoot()
+		$ak47.play("default")
 	else:
 		counter = 0
 		$shot_timer.stop()
 		$reload_timer.start()
+		$ak47.stop()
+		$ak47.frame = 0
 
 func shoot():
 	var stage_node = get_parent()
 	var shot_instance = shot_scene.instance()
 	shot_instance.position = position - Vector2(32, -6)
 	stage_node.add_child(shot_instance)
+	var casing_instance = casing_scene.instance()
+	rng.randomize()
+	casing_instance.position = position + Vector2(rng.randi_range(48, 16), rng.randi_range(16, 32))
+	stage_node.add_child(casing_instance)
 	
 func _on_reload_timer_timeout():
 	$shot_timer.start()
